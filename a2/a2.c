3812c2f22ceab89e7d0c6b424d069a9eecde20f1
Malachi Bergman <malachijb28@gmail.com>
2022-10-28 20:29:17 -0400

    initial move around

---
+++ a2/a2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void main(int argc, char **argv){
+    int quantum = atoi(argv[2]);
+    char *inputFilename = argv[1];
+
+
+}
---
+++ prime.c
@@ -0,0 +1,5 @@
+//
+// Created by malachi.bergman on 10/28/2022.
+//
+
+void main(int )
