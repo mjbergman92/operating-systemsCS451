619a30c22db65c8f1c78adfc050af1fca0937c6d
Malachi Bergman <malachijb28@gmail.com>
2022-10-29 10:05:20 -0400

    clion works with linux! on windows

--- prime/prime.c
+++ prime/prime.c
@@ -2,6 +2,19 @@
 // Created by malachi.bergman on 10/28/2022.
 //
 
+#include <signal.h>
+
+void sigtstp(int signal){
+
+}
+void sigcont(int signal){
+
+}
+void sigterm(int signal){
+
+}
+
 void main(int argc, char **argv){
+    signal(SIGTSTP, sigtstp);
 
 }
