affb6cb19fb95c23aa658d95555834253029318a
Malachi Bergman <malachijb28@gmail.com>
2022-11-01 09:41:34 -0400

    initial signal handling

--- a2/a2.c
+++ a2/a2.c
@@ -1,33 +1,133 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/time.h>
+#include <signal.h>
+#include <unistd.h>
 #include "pid.h"
 
 #define LINE_MAX 40
 
+Queue *queue = NULL;
+PID *pid = NULL;
+int quantum;
+int loops = 0;
+int doPrint = 0;
+int prevPID = -1;
+
+void sigalrm(int signal){
+    // suspend or terminate currently running process, if there is one running
+    prevPID = -1;
+    if(pid != NULL){
+        prevPID = pid->namePID;
+        pid->burst -= quantum;
+        if(pid->burst <= 0){
+            kill(pid->pid, SIGTERM);
+            free(pid);
+        }else{
+            kill(pid->pid, SIGTSTP);
+            enqueuePID(queue, pid);
+        }
+    }
+
+    pid = dequeuePID(queue);
+
+    //check if all processes have left the queue
+    if(pid == NULL){
+        freeQueue(queue);
+        exit(0);
+    }
+
+    // start or resume next process
+    if(pid->pid == -1){
+        int p = fork();
+        if(p == 0){
+            char pid_str[6];
+            int __pid = pid->namePID;
+            int i;
+            for(i = 0; i < 6 - 1 && __pid > 0; i++){
+                int digit = __pid % 10;
+                __pid /= 10;
+                pid_str[i] = digit + '0';
+            }
+            pid_str[i] = '\0';
+            if(execl("./prime/cmake-build-debug/prime", "prime", pid_str, NULL) == -1){
+                exit(-1);
+            }
+        }
+        pid->pid = p;
+    }else{
+        kill(pid->pid, SIGCONT);
+    }
+
+    doPrint = 1;
+    loops++;
+}
+
+void setupTimer(){
+    struct itimerval timer;
+    /* Install timer_handler as the signal handler for SIGALRM. */
+    /* The timer goes off 10 seconds after installation of the timer*/
+    timer.it_value.tv_sec = quantum;
+    timer.it_value.tv_usec =0;
+    /* ... and every 10 seconds after that. */
+    timer.it_interval.tv_sec = quantum;
+    timer.it_interval.tv_usec =0;
+    /* Start a real timer. It counts down whenever this process is executing. */
+    setitimer (ITIMER_REAL, &timer, NULL);
+}
+
+void setupAlarm(){
+    struct sigaction sa;
+    /* Install timer_handler as the signal handler for SIGALRM. */
+    memset (&sa, 0, sizeof (sa));
+    sa.sa_handler = &sigalrm;
+    sigaction(SIGALRM, &sa, NULL);
+}
+
 void main(int argc, char **argv){
     char *inputFilename = argv[1];
-    int quantum = atoi(argv[2]);
+    quantum = atoi(argv[2]);
 
-    printf("Quantum: %d\n", quantum);
-    printf("Input File: %s\n", inputFilename);
+    //printf("Quantum: %d\n", quantum);
+    //printf("Input File: %s\n", inputFilename);
 
     FILE *fp = fopen(inputFilename, "r");
 
     //skip the header line
     char input[LINE_MAX];
     fgets(input, LINE_MAX, fp);
-    printf("Skipped: %s\n", input);
+    //printf("Skipped: %s\n", input);
 
-    Queue *queue = createQueue();
-
+    // parse input file into queue
+    queue = createQueue();
     int namePID, burst;
     while(fscanf(fp, "%d%d", &namePID, &burst) == 2){
-        PID *pid = createPID(namePID, burst);
-        enqueuePID(queue, pid);
+        PID *_pid = createPID(namePID, burst);
+        enqueuePID(queue, _pid);
     }
 
-    queueToString(queue);
+    fclose(fp);
 
-    freeQueue(queue);
+    // queueToString(queue);
+
+    setupTimer();
+    setupAlarm();
+
+    while(1) {
+        if(doPrint){
+            printf("\nScheduler: Time Now: %d second\n", loops * quantum);
+
+            if(prevPID == -1) {
+                //starting process when pid == NULL
+                printf( "Scheduling to Process %d (Pid %d) for the time slice of %d seconds.\n", pid->namePID, pid->pid, quantum);
+            }else{
+                //for processes with pid != NULL
+                printf("Suspending Process %d and scheduling Process %d (Pid %d) for the time\n"
+                                       "slice of %d seconds.\n", prevPID, pid->namePID, pid->pid, quantum);
+            }
+
+            doPrint = 0;
+        }
+    };
 }
--- prime/prime.c
+++ prime/prime.c
@@ -3,20 +3,107 @@
 //
 
 #include <signal.h>
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+long unsigned int prime = 0;
+int namePID, pid;
+int printFlag = 0;
 
 void sigtstp(int signal){
-
+    printFlag = 1;
 }
 void sigcont(int signal){
-
+    printFlag = 2;
 }
 void sigterm(int signal){
+    printFlag = 3;
+}
 
+long unsigned int get10DigitRandom(){
+    srand(time(NULL));
+
+    long unsigned int output = 0;
+
+    for(int i = 0; i < 10; i++){
+        int r = rand() % 10;
+        output = output * 10 + r;
+    }
+
+    return output;
+}
+
+int checkPrime(long unsigned int number){
+    for(long unsigned int i = 2; i < number; i++){
+        if(number % i == 0){
+            return 0;
+        }
+    }
+    return 1;
+}
+
+void setupSignals(){
+    struct sigaction sa1, sa2, sa3;
+    /* Install timer_handler as the signal handler for SIGALRM. */
+    memset (&sa1, 0, sizeof (sa1));
+    sa1.sa_flags = SA_RESTART;
+    sa1.sa_handler = &sigtstp;
+    sigaction(SIGTSTP, &sa1, NULL);
+
+    /* Install timer_handler as the signal handler for SIGALRM. */
+    memset (&sa2, 0, sizeof (sa2));
+    sa2.sa_flags = SA_RESTART;
+    sa2.sa_handler = &sigcont;
+    sigaction(SIGCONT, &sa2, NULL);
+
+    /* Install timer_handler as the signal handler for SIGALRM. */
+    memset (&sa3, 0, sizeof (sa3));
+    sa3.sa_flags = SA_RESTART;
+    sa3.sa_handler = &sigterm;
+    sigaction(SIGTERM, &sa3, NULL);
 }
 
 void main(int argc, char **argv){
-    signal(SIGTSTP, sigtstp);
-    signal(SIGCONT, sigcont);
-    signal(SIGTERM, sigterm);
+    setupSignals();
 
+    long unsigned int number = get10DigitRandom();
+
+    namePID = atoi(argv[1]);
+    pid = getpid();
+
+    //initial message when prime process started
+    printf("Process %d: my PID is %d: I just got started. I am starting with the\n"
+           "number %lu to find the next prime number.\n", namePID, pid, number);
+
+    while(1){
+        if(checkPrime(number)){
+            prime = number;
+        }
+        number++;
+
+        switch(printFlag){
+            case 1:
+                printf("Process %d: my PID is %d: I am about to be suspended... Highest prime\n"
+                    "number I found is %lu.\n", namePID, pid, prime);
+                break;
+            case 2:
+                printf("Process %d: my PID is %d: I just got resumed.\n", namePID, pid);
+                break;
+            case 3:
+                printf("Process %d: my PID is %d: I completed my task and I am exiting.\n"
+                    "Highest prime number I found is %lu.\n", namePID, pid, prime);
+                break;
+            default:
+                break;
+        }
+
+        printFlag = 0;
+    }
 }
+
+
+
+
